1835d4168a0ae1906abd8d6468c1667cdd22d93c
Mateo Cabrera <mateo.cabrera@epitech.eu>
2025-07-20 14:08:42 +0200

    fix(HUD): Align update_lives_display signature with signal

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,10 +3,10 @@ extends CanvasLayer
 @onready var hearts: Array[Sprite2D] = [$LivesContainer/Heart1, $LivesContainer/Heart2, $LivesContainer/Heart3]
 
 func _ready():
-	update_lives_display()
+	update_lives_display(GameState.get_current_lives())
+
 	GameState.lives_changed.connect(update_lives_display)
 
-func update_lives_display():
-	var current_lives = GameState.get_current_lives()
+func update_lives_display(current_lives: int):
 	for i in range(hearts.size()):
 		hearts[i].visible = (i < current_lives)
